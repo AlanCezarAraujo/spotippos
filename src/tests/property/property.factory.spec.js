0acbcf5babebe664830f216efd74f1876c81348e
Alan Cezar <alan.cezar.silva@gmail.com>
2016-09-15 00:35:04 -0300

    Criados testes unitários para property.factory.js

--- src/tests/property/property.factory.spec.js
+++ src/tests/property/property.factory.spec.js
@@ -6,7 +6,13 @@ import PropertyFactory from '../../js/modules/property/property.factory';
 chai.use(spies);
 chai.should();
 
-var $http;
+var $http = {
+    get: () => {
+        return {
+            success: () => {}
+        }
+    }
+};
 
 var factory = new PropertyFactory($http);
 
@@ -14,5 +20,24 @@ describe('The PropertyFactory', () => {
     it('should initialize an empty properties list', () => {
         expect(factory.properties).to.be.a('array');
         expect(factory.properties.length).to.equal(0);
+    });
+
+    it('should get properties from the server', () => {
+        const get = chai.spy.on($http, 'get');
+
+        factory.listAllProperties();
+
+        expect(get).to.have.been.called;
+    });
+
+    it('should add 30 items from a cached list when asked to', () => {
+        factory.chachedProperties = new Array(100);
+        factory.properties = [];
+
+        expect(factory.properties.length).to.equal(0);
+
+        factory.getMoreProperties();
+
+        expect(factory.properties.length).to.equal(30);
     });
 });
